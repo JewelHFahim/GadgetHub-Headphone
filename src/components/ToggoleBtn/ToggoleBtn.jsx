d0c35193d2494b57f7c5470af6489e70f8be22c6
Jewel Hossain Fahim <76914444+JewelHFahim@users.noreply.github.com>
2023-07-27 10:49:12 +0600

    stored theme status on localstorage

--- src/components/ToggoleBtn/ToggoleBtn.jsx
+++ src/components/ToggoleBtn/ToggoleBtn.jsx
@@ -1,40 +1,40 @@
 import { ThemeContext } from '@/Context/AppContext';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 
-
 const ToggleBtn = () => {
-    const { isDarkmode, setIsDarkmode } = useContext(ThemeContext)
+    const { isDarkmode, setIsDarkmode } = useContext(ThemeContext);
 
-    const darkIcon = (
-        <BsFillMoonFill className='text-[20px]' />
-    );
+    const darkIcon = <BsFillMoonFill className='text-[20px]' />;
+    const lightIcon = <BsFillSunFill className='text-[20px]' />;
 
-    const lightIcon = (
-        <BsFillSunFill className='text-[20px]' />
-    );
+    useEffect(() => {
+        const storedStatus = JSON.parse(localStorage.getItem('status')) || false;
 
+        setIsDarkmode(storedStatus);
+    }, [setIsDarkmode]);
 
     function toggleTheme() {
         setIsDarkmode(!isDarkmode);
+
+        localStorage.setItem('status', JSON.stringify(!isDarkmode));
     }
 
     return (
-        <div className=' lg:rotate-[-90deg]'>
-
+        <div className='lg:rotate-[-90deg]'>
             <button
-                className={`w-[70px] h-[30px] rounded-full bg-[#0F121A] flex items-center transition duration-300 border border-white border-opacity-[10%]  focus:outline-none shadow ${isDarkmode ? 'dark' : ''
-                    }`} onClick={toggleTheme}>
-
-                <div id="switch-toggle"
-                    className={`w-[35px] h-[35px] relative rounded-full transition duration-500 transform ${isDarkmode ? 'bg-gray-700 translate-x-full' : 'bg-yellow-500 -translate-x-2'
-                        } p-1 text-white flex justify-center items-center`}>
-
+                className={`lg:w-[70px] lg:h-[30px] rounded-full lg:bg-[#0F121A] flex items-center transition duration-300 border border-white border-opacity-[10%] focus:outline-none shadow ${isDarkmode ? 'dark' : ''
+                    }`}
+                onClick={toggleTheme}
+            >
+                <div
+                    id='switch-toggle'
+                    className={`w-[35px] h-[35px] relative rounded-full transition duration-500 transform ${isDarkmode ? 'bg-gray-700 lg:translate-x-full' : 'bg-yellow-500 lg:-translate-x-2'
+                        } p-1 text-white flex justify-center items-center`}
+                >
                     {isDarkmode ? darkIcon : lightIcon}
-
                 </div>
             </button>
-
         </div>
     );
 };
